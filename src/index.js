e034688abf3bc62783b97898c86893abf4b26bd0
= <=>
2020-05-21 12:26:00 +1000

    Added loop checkbox

--- src/index.js
+++ src/index.js
@@ -3,18 +3,30 @@ import './index.css';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-
     onButtonPress() {
+        console.log("Button pressed");
         // Play a sound
-        let audio = new Audio('./MissyBeats.wav')
-        audio.play()
-        console.log("Button pressed")
+        let audio = new Audio('./MissyBeats.wav');
+
+        if (document.getElementById("loop").checked === true) {
+            audio.loop = true;
+        } else {
+            audio.loop = false;
+        }
+
+        audio.play();
     }
 
     render() {
-        return(
-            <div className="machine" id="display" onClick={this.onButtonPress}>
-                <h1>Drop the beat</h1>
+        return (
+            <div>
+                <div className="machine" id="display" onClick={this.onButtonPress}>
+                    <h1>Drop the beat</h1>
+                </div>
+                <div className="controls">
+                    <label htmlFor="loop">Loop - </label>
+                    <input className="checkBox" type="checkbox" id="loop"/>
+                </div>
             </div>
         )
     }
