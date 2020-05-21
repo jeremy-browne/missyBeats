e91a80b841e370abf3b93325a45c4266c352730d
= <=>
2020-05-21 12:57:04 +1000

    Made some things

--- src/index.js
+++ src/index.js
@@ -3,25 +3,52 @@ import './index.css';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-    onButtonPress() {
-        console.log("Button pressed");
-        // Play a sound
-        let audio = new Audio('./MissyBeats.wav');
+    constructor(props) {
+        super(props);
+        this.state = {
+            buttonStatus: "Drop the beat",
+            playAudio: true,
+        }
+    }
 
+    togglePlay() {
+        if (this.state.playAudio === false) {
+            this.setState({
+                buttonStatus: "Stop",
+                playAudio: false,
+            })
+        } else {
+            this.setState({
+                buttonStatus: "Drop the beat",
+                playAudio: true,
+            })
+        }
+    }
+
+    playAudio() {
+        console.log("Button pressed");
+        let audio = new Audio('./MissyBeats.wav');
+        
+        this.togglePlay();
+        
+        // Play a sound
         if (document.getElementById("loop").checked === true) {
             audio.loop = true;
         } else {
             audio.loop = false;
         }
 
-        audio.play();
+        if(this.state.playAudio === true) {
+            audio.play();
+        }
+        console.log(this.state);
     }
 
     render() {
         return (
             <div>
-                <div className="machine" id="display" onClick={this.onButtonPress}>
-                    <h1>Drop the beat</h1>
+                <div className="machine" id="display" onClick={() => this.playAudio()}>
+                    <h1>{this.state.buttonStatus}</h1>
                 </div>
                 <div className="controls">
                     <label htmlFor="loop">Loop - </label>
